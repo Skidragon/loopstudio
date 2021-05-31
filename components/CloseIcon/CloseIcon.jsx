6c4662f8d80b2d92d16c607c488deffaf034a39a
Simon Davis <simonfreelance1@gmail.com>
2021-05-31 06:46:17 -0400

    Removed alt from CloseIcon

--- components/CloseIcon/CloseIcon.jsx
+++ components/CloseIcon/CloseIcon.jsx
@@ -9,7 +9,6 @@ export default function CloseIcon({ ...props }) {
       xmlns="http://www.w3.org/2000/svg"
       viewBox={"0 0 21 21"}
       tabIndex="-1"
-      alt
       {...props}
       className={cn({
         [styles["close-icon"]]: true,
