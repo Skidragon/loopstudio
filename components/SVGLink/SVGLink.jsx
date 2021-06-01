e515f6d829c89f79bcc60423db594e196832a2d4
Simon Davis <simonfreelance1@gmail.com>
2021-06-01 14:18:02 -0400

    Finished Footer styles

--- components/SVGLink/SVGLink.jsx
+++ components/SVGLink/SVGLink.jsx
@@ -1,10 +1,10 @@
 import Link from "next/link";
 import React from "react";
-
+import styles from "./SVGLink.module.scss";
 export const SVGLink = ({ href = "", svgElement, ...props }) => {
   return (
     <Link href={href}>
-      <a>{React.cloneElement(svgElement)}</a>
+      <a className={styles["svg-link"]}>{React.cloneElement(svgElement)}</a>
     </Link>
   );
 };
